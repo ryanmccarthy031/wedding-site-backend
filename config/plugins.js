3dc2fdfd0439e39b57d724cf7fb6400a4094431f
Ryan McCarthy <ryanmccarthy031@gmail.com>
2020-09-07 15:36:33 -0400

    Changing back to second provider

--- config/plugins.js
+++ config/plugins.js
@@ -1,25 +1,27 @@
 module.exports = ({ env }) => ({
-    upload: {
-      provider: 'aws-s3',
-      providerOptions: {
-        accessKeyId: env('AWS_KEY'),
-        secretAccessKey: env('AWS_SECRET'),
-        region: env('AWS_REGION'),
-        params: {
-          Bucket: env('AWS_BUCKET'),
-        },
+  upload: {
+    provider: 'aws-s3',
+    providerOptions: {
+      accessKeyId: env('AWS_KEY'),
+      secretAccessKey: env('AWS_SECRET'),
+      region: env('AWS_REGION'),
+      params: {
+        Bucket: env('AWS_BUCKET'),
       },
     },
-    email: {
-      provider: 'gmail-2lo',
-      providerOptions: {
-        username: 'us@leighandryan.com',
-        clientId: env('EMAIL_CLIENT_ID'),
-        privateKey: env('EMAIL_PRIVATE_KEY'),
-      },
-      settings: {
-        defaultFrom: 'us@leighandryan.com',
-        defaultReplyTo: 'us@leighandryan.com',
-      },
+  },
+  email: {
+    provider: 'gmail-oauth2',
+    providerOptions: {
+      username: env('SERVICE_ACCOUNT_EMAIL'),
+      clientId: env('EMAIL_CLIENT_ID'),
+      clientSecret: env('EMAIL_PRIVATE_KEY'),
+      refreshToken: env('EMAIL_REFRESH_TOKEN'),
+      accessToken: env('EMAIL_ACCESS_TOKEN'),
     },
-  });
+    settings: {
+      defaultFrom: env('GMAIL_ADDR'),
+      defaultReplyTo: env('GMAIL_ADDR'),
+    },
+  },
+});
