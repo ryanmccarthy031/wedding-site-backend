a039e54a5a26f42ac16a246354113d106e88c7fb
Ryan McCarthy <ryanmccarthy031@gmail.com>
2020-09-07 14:51:37 -0400

    Added some cors middleware

---
+++ config/middleware.js
@@ -0,0 +1,7 @@
+module.exports = {
+    settings: {
+      cors: {
+        origin: ['http://staging.leighandryan.com', 'https://leighandryan.com', 'https://www.leighandryan.com'],
+      },
+    },
+};
